8d8d4bfb2b89b4679b07ab58b4e98a8732dcc11e
Wai Shing CHAN <cws265@corp.ha.org.hk>
2025-09-26 11:44:41 +0800

    Update vite.config.ts and add static build for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -15,4 +14,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/webcam-qr-scan", // Added for GitHub Pages
 }));
